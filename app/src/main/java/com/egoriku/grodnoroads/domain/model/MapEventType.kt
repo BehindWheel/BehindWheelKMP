68ff94eb55e9acecc5bf687ea78ac2150fdb7197
yahor_urbanovich <egorikftp@gmail.com>
2022-05-27 22:43:52 +0300

    Add more Map Events

--- app/src/main/java/com/egoriku/grodnoroads/domain/model/MapEventType.kt
+++ app/src/main/java/com/egoriku/grodnoroads/domain/model/MapEventType.kt
@@ -1,10 +1,18 @@
 package com.egoriku.grodnoroads.domain.model
 
-enum class MapEventType(val type: String) {
-    StationaryCamera(type = "stationary"),
-    MobileCamera(type = "mobile"),
-    TrafficPolice(type = "police"),
-    RoadAccident(type = "accident");
+enum class MapEventType(
+    val type: String,
+    val emoji: String
+) {
+    StationaryCamera(type = "stationary", emoji = "📷"),
+    MobileCamera(type = "mobile", emoji = "📸"),
+    TrafficPolice(type = "police", emoji = "👮"),
+
+    RoadAccident(type = "accident", emoji = "❗"),
+    RoadRepair(type = "road_repair", emoji = "🚧"),
+    TrafficJam(type = "traffic_jam", emoji = "🚗🚕🚛"),
+    WildAnimals(type = "wild_animals", emoji = "🦌"),
+    CarCrash(type = "car_crash", emoji = "💥🚗");
 
     companion object {
         fun eventFromString(value: String): MapEventType {
