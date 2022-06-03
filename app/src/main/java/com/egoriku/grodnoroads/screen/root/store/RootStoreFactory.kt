724f841e4fc8a211e111d44935f407e575dc11d5
yahor_urbanovich <egorikftp@gmail.com>
2022-06-03 17:30:21 +0300

    Remove unused Intent in RootStoreFactory

--- app/src/main/java/com/egoriku/grodnoroads/screen/root/store/RootStoreFactory.kt
+++ app/src/main/java/com/egoriku/grodnoroads/screen/root/store/RootStoreFactory.kt
@@ -7,7 +7,6 @@ import com.arkivanov.mvikotlin.core.store.Store
 import com.arkivanov.mvikotlin.core.store.StoreFactory
 import com.arkivanov.mvikotlin.core.utils.ExperimentalMviKotlinApi
 import com.arkivanov.mvikotlin.extensions.coroutines.coroutineExecutorFactory
-import com.egoriku.grodnoroads.screen.root.store.RootStoreFactory.Intent
 import com.egoriku.grodnoroads.screen.root.store.RootStoreFactory.State
 import com.egoriku.grodnoroads.screen.settings.domain.Theme
 import com.egoriku.grodnoroads.screen.settings.store.preferences.APP_THEME
@@ -15,14 +14,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
-interface RootStore : Store<Intent, State, Nothing>
+interface RootStore : Store<Nothing, State, Nothing>
 
 class RootStoreFactory(
     private val storeFactory: StoreFactory,
     private val dataStore: DataStore<Preferences>
 ) {
 
-    sealed interface Intent
     sealed interface Message {
         data class NewTheme(val theme: Theme) : Message
     }
@@ -31,7 +29,7 @@ class RootStoreFactory(
 
     @OptIn(ExperimentalMviKotlinApi::class)
     fun create(): RootStore =
-        object : RootStore, Store<Intent, State, Nothing> by storeFactory.create(
+        object : RootStore, Store<Nothing, State, Nothing> by storeFactory.create(
             initialState = State(),
             executorFactory = coroutineExecutorFactory(Dispatchers.Main) {
                 onAction<Unit> {
