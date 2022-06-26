c4e4c2e3a81fc6f93eae0ff3befb8b6835d75342
yahor_urbanovich <egorikftp@gmail.com>
2022-06-26 17:15:54 +0300

    add EmptyLatLng

--- app/src/main/java/com/egoriku/grodnoroads/screen/map/domain/LocationState.kt
+++ app/src/main/java/com/egoriku/grodnoroads/screen/map/domain/LocationState.kt
@@ -7,9 +7,12 @@ data class LocationState(
     val bearing: Float,
     val speed: Int
 ) {
+
     companion object {
+        val EmptyLatLng = LatLng(0.0, 0.0)
+
         val None = LocationState(
-            latLng = LatLng(0.0, 0.0),
+            latLng = EmptyLatLng,
             bearing = 0f,
             speed = 0
         )
