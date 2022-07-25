3b69d98c3496dbb3e6adb2109a49ad4849aae566
yahor_urbanovich <egorikftp@gmail.com>
2022-07-25 17:02:56 +0300

    Remove DrawerNavigationItem in favour of ListItem

--- app/src/main/java/com/egoriku/grodnoroads/screen/main/ui/drawer/section/ShareAppSection.kt
+++ app/src/main/java/com/egoriku/grodnoroads/screen/main/ui/drawer/section/ShareAppSection.kt
@@ -1,33 +1,36 @@
 package com.egoriku.grodnoroads.screen.main.ui.drawer.section
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.clickable
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
+import androidx.compose.material.ListItem
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.egoriku.grodnoroads.R
-import com.egoriku.grodnoroads.foundation.drawer.DrawerNavigationItem
 import com.egoriku.grodnoroads.ui.theme.GrodnoRoadsTheme
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ShareAppSection(onClick: (String) -> Unit) {
     val url = stringResource(R.string.play_store_link)
 
-    DrawerNavigationItem(
-        text = stringResource(R.string.drawer_share_app_section),
+    ListItem(
+        modifier = Modifier.clickable { onClick(url) },
+        text = {
+            Text(text = stringResource(R.string.drawer_share_app_section))
+        },
         icon = {
             Icon(
-                modifier = Modifier.size(24.dp),
                 imageVector = Icons.Default.Share,
                 contentDescription = null
             )
         },
-        onClick = { onClick(url) }
     )
 }
 
--- app/src/main/java/com/egoriku/grodnoroads/screen/main/ui/drawer/section/TelegramSection.kt
+++ app/src/main/java/com/egoriku/grodnoroads/screen/main/ui/drawer/section/TelegramSection.kt
@@ -2,7 +2,11 @@ package com.egoriku.grodnoroads.screen.main.ui.drawer.section
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ListItem
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -10,24 +14,24 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.egoriku.grodnoroads.R
-import com.egoriku.grodnoroads.foundation.drawer.DrawerNavigationItem
 import com.egoriku.grodnoroads.ui.theme.GrodnoRoadsTheme
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun TelegramSection(onClick: (String) -> Unit) {
     val url = stringResource(R.string.tg_link)
 
-    DrawerNavigationItem(
-        text = stringResource(R.string.drawer_telegram_section),
+    ListItem(
+        modifier = Modifier.clickable { onClick(url) },
+        text = {
+            Text(text = stringResource(R.string.drawer_telegram_section))
+        },
         icon = {
             Image(
                 modifier = Modifier.size(28.dp),
                 painter = painterResource(R.drawable.ic_telegram),
                 contentDescription = null
             )
-        },
-        onClick = {
-            onClick(url)
         }
     )
 }
