c1116d9d85edb2d51104b6181f39ed4544df29a5
egorikftp <egorikftp@gmail.com>
2022-10-30 21:53:06 +0300

    Add new supported cities

--- shared/appSettings/src/main/kotlin/com/egoriku/grodnoroads/shared/appsettings/types/map/location/City.kt
+++ shared/appSettings/src/main/kotlin/com/egoriku/grodnoroads/shared/appsettings/types/map/location/City.kt
@@ -9,9 +9,20 @@ enum class City(val cityName: String, val latLng: LatLng) {
     Volkovysk(cityName = "volkovysk", latLng = LatLng(53.152847, 24.444242)),
     Ozery(cityName = "ozery", latLng = LatLng(53.722526, 24.178165)),
     Porechye(cityName = "porechye", latLng = LatLng(53.885623, 24.137678)),
-    Berestovitca(cityName = "berestovitca", latLng = LatLng(53.191924, 24.018453));
+    Berestovitca(cityName = "berestovitca", latLng = LatLng(53.191924, 24.018453)),
+    Shuchin(cityName = "shuchin", latLng = LatLng(53.604412, 24.742567));
 
     companion object {
+        val supportedCities = listOf(
+            Berestovitca,
+            Volkovysk,
+            Grodno,
+            Ozery,
+            Porechye,
+            Skidel,
+            Shuchin,
+        )
+
         fun toCity(value: String) =
             checkNotNull(values().find { it.cityName == value })
 
@@ -22,6 +33,7 @@ enum class City(val cityName: String, val latLng: LatLng) {
             Ozery -> R.string.map_default_location_ozery
             Porechye -> R.string.map_default_location_porechye
             Berestovitca -> R.string.map_default_location_berestovitca
+            Shuchin -> R.string.map_default_location_shuchin
         }
     }
 }
