1fcd4e7678af4c700af49e81f4b7565bf6dcf5e8
egorikftp <egorikftp@gmail.com>
2023-06-21 15:50:59 +0300

    Use FilterChip instead SegmentedButton

--- features/map/map-ui/src/main/kotlin/com/egoriku/grodnoroads/map/popup/ActionsContent.kt
+++ features/map/map-ui/src/main/kotlin/com/egoriku/grodnoroads/map/popup/ActionsContent.kt
@@ -1,10 +1,9 @@
 package com.egoriku.grodnoroads.map.popup
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -14,9 +13,6 @@ import androidx.compose.ui.unit.dp
 import com.egoriku.grodnoroads.foundation.core.rememberMutableState
 import com.egoriku.grodnoroads.foundation.theme.GrodnoRoadsM3ThemePreview
 import com.egoriku.grodnoroads.foundation.theme.GrodnoRoadsPreview
-import com.egoriku.grodnoroads.foundation.uikit.button.SegmentedButton
-import com.egoriku.grodnoroads.foundation.uikit.button.SegmentedButtonDefaults
-import com.egoriku.grodnoroads.foundation.uikit.button.SegmentedButtonRow
 import com.egoriku.grodnoroads.map.domain.store.quickactions.model.QuickActionsPref
 import com.egoriku.grodnoroads.map.domain.store.quickactions.model.QuickActionsPref.*
 import com.egoriku.grodnoroads.map.domain.store.quickactions.model.QuickActionsState
@@ -32,27 +28,33 @@ fun ActionsContent(
     Column {
         Text(
             modifier = Modifier.padding(bottom = 4.dp),
+            style = MaterialTheme.typography.titleSmall,
             text = stringResource(R.string.appearance_app_theme)
         )
 
         val appTheme = quickActionsState.appTheme
-        SegmentedButtonRow {
-            appTheme.values.forEachIndexed { index, theme ->
-                val checked = appTheme.current == theme
-                SegmentedButton(
-                    colors = SegmentedButtonDefaults.colors(
-                        checkedContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.3f),
-                    ),
-                    checked = checked,
-                    shape = SegmentedButtonDefaults.shape(
-                        position = index,
-                        count = appTheme.values.size
-                    ),
+
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            appTheme.values.forEach { theme ->
+                val selected = appTheme.current == theme
+                FilterChip(
+                    label = {
+                        Text(text = stringResource(theme.toStringResource()))
+                    },
+                    selected = selected,
+                    leadingIcon = if (selected) {
+                        {
+                            Icon(
+                                imageVector = Icons.Filled.Done,
+                                contentDescription = "Localized Description",
+                                modifier = Modifier.size(FilterChipDefaults.IconSize)
+                            )
+                        }
+                    } else {
+                        null
+                    },
                     onClick = {
                         onChanged(appTheme.copy(current = theme))
-                    },
-                    content = {
-                        Text(text = stringResource(theme.toStringResource()))
                     }
                 )
             }
