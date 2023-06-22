c56e9be7ad3970567d89e1c90662426fba53bb34
Yahor Urbanovich <egorikftp@gmail.com>
2023-06-22 11:22:23 +0300

    Adjust over speed logic

--- features/map/map-domain/src/main/kotlin/com/egoriku/grodnoroads/map/domain/util/OverSpeedTransformation.kt
+++ features/map/map-domain/src/main/kotlin/com/egoriku/grodnoroads/map/domain/util/OverSpeedTransformation.kt
@@ -6,21 +6,20 @@ import com.egoriku.grodnoroads.map.domain.model.LastLocation
 private const val GEO_SPEED_ACCURACY = 4
 private const val ALLOWED_OVER_SPEED = 5
 
-fun overSpeedTransformation(): suspend (
-    List<Alert>, LastLocation
-) -> Int = { alerts, lastLocation ->
-    val cameraEvents = alerts.filterIsInstance<Alert.CameraAlert>()
+fun overSpeedTransformation(): suspend (List<Alert>, LastLocation) -> Int =
+    { alerts, lastLocation ->
+        val cameraEvents = alerts.filterIsInstance<Alert.CameraAlert>()
 
-    if (cameraEvents.isEmpty()) {
-        -1
-    } else {
-        val speedLimit = cameraEvents.first().speedLimit
-        val currentSpeed = lastLocation.speed
-
-        if (currentSpeed + GEO_SPEED_ACCURACY + ALLOWED_OVER_SPEED >= speedLimit) {
-            speedLimit
-        } else {
+        if (cameraEvents.isEmpty()) {
             -1
+        } else {
+            val speedLimit = cameraEvents.first().speedLimit
+            val currentSpeed = lastLocation.speed
+
+            if (currentSpeed + GEO_SPEED_ACCURACY >= speedLimit + ALLOWED_OVER_SPEED) {
+                speedLimit
+            } else {
+                -1
+            }
         }
     }
-}
