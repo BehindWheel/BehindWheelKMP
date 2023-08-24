1c8f50c285d994cdc759637e3e3e0faf57eabc62
Yahor Urbanovich <egorikftp@gmail.com>
2023-08-24 10:46:12 +0300

    Update bottom navigation icons and colors

--- app/src/main/java/com/egoriku/grodnoroads/screen/main/MainUi.kt
+++ app/src/main/java/com/egoriku/grodnoroads/screen/main/MainUi.kt
@@ -9,12 +9,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.jetpack.stack.Children
 import com.arkivanov.decompose.extensions.compose.jetpack.subscribeAsState
 import com.arkivanov.decompose.router.stack.ChildStack
+import com.egoriku.grodnoroads.foundation.theme.isLight
 import com.egoriku.grodnoroads.map.MapScreen
 import com.egoriku.grodnoroads.screen.main.MainComponent.Child
 import com.egoriku.grodnoroads.setting.screen.SettingsScreen
@@ -61,14 +62,20 @@ private fun VerticalOrientationLayout(
             }
         }
 
-        NavigationBar(tonalElevation = 1.dp) {
+        val tonalElevation = if (MaterialTheme.colorScheme.isLight) 0.dp else 6.dp
+        NavigationBar(tonalElevation = tonalElevation) {
             bottomNavItems.forEach { screen ->
                 NavigationBarItem(
                     selected = screen.index == childStack.active.instance.index,
                     onClick = { component.onSelectTab(index = screen.index) },
+                    colors = NavigationBarItemDefaults.colors(
+                        unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.45f),
+                        unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.45f),
+                        indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(tonalElevation)
+                    ),
                     icon = {
                         Icon(
-                            painter = rememberVectorPainter(image = screen.icon),
+                            painter = painterResource(id = screen.iconRes),
                             contentDescription = null
                         )
                     },
@@ -89,19 +96,24 @@ private fun HorizontalOrientationLayout(
     val bottomNavItems = remember { listOf(Screen.Map, Screen.Settings) }
 
     Row(modifier = Modifier.fillMaxSize()) {
-        NavigationRail {
+        val tonalElevation = if (MaterialTheme.colorScheme.isLight) 0.dp else 6.dp
+        val containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(tonalElevation)
+
+        NavigationRail(containerColor = containerColor) {
             bottomNavItems.forEach { screen ->
                 NavigationRailItem(
+                    colors = NavigationRailItemDefaults.colors(
+                        unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.45f),
+                        unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.45f),
+                        indicatorColor = containerColor
+                    ),
                     selected = screen.index == childStack.active.instance.index,
                     onClick = { component.onSelectTab(index = screen.index) },
                     icon = {
                         Icon(
-                            painter = rememberVectorPainter(image = screen.icon),
+                            painter = painterResource(id = screen.iconRes),
                             contentDescription = null
                         )
-                    },
-                    label = {
-                        Text(text = stringResource(id = screen.labelId))
                     }
                 )
             }
