e7704fed9b22c49f0bddef6f12f123e5837e8d37
Yahor Urbanovich <egorikftp@gmail.com>
2023-12-05 18:01:32 +0300

    Add more states for Checkbox

--- ui-demo/src/main/kotlin/com/egoriku/grodnoroads/uidemo/ui/demo/DemoCheckbox.kt
+++ ui-demo/src/main/kotlin/com/egoriku/grodnoroads/uidemo/ui/demo/DemoCheckbox.kt
@@ -2,7 +2,9 @@ package com.egoriku.grodnoroads.uidemo.ui.demo
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.egoriku.grodnoroads.foundation.core.rememberMutableState
 import com.egoriku.grodnoroads.foundation.theme.GrodnoRoadsDarkLightPreview
@@ -13,22 +15,31 @@ import com.egoriku.grodnoroads.uidemo.ui.UIDemoContainer
 @Composable
 fun DemoCheckbox() {
     UIDemoContainer(name = "Checkbox") {
-        val (checkbox1, onChange1) = rememberMutableState { true }
-        val (checkbox2, onChange2) = rememberMutableState { false }
-
         Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            val (isChecked, onCheckedChange) = rememberMutableState { true }
+
             Checkbox(
-                checked = checkbox1,
-                onCheckedChange = onChange1
+                checked = isChecked,
+                onCheckedChange = onCheckedChange
             )
+            Spacer(modifier = Modifier.weight(1f))
             Checkbox(
-                checked = checkbox2,
-                onCheckedChange = onChange2
+                checked = true,
+                onCheckedChange = {}
             )
             Checkbox(
                 checked = true,
-                onCheckedChange = {},
-                enabled = false
+                enabled = false,
+                onCheckedChange = {}
+            )
+            Checkbox(
+                checked = false,
+                onCheckedChange = {}
+            )
+            Checkbox(
+                checked = false,
+                enabled = false,
+                onCheckedChange = {}
             )
         }
     }
