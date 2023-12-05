7f6ee9630cb7c5a8eee15a27e4b733850723862c
Yahor Urbanovich <egorikftp@gmail.com>
2023-12-05 18:03:47 +0300

    Add more states for RadioButton

--- ui-demo/src/main/kotlin/com/egoriku/grodnoroads/uidemo/ui/demo/DemoRadioButton.kt
+++ ui-demo/src/main/kotlin/com/egoriku/grodnoroads/uidemo/ui/demo/DemoRadioButton.kt
@@ -2,9 +2,11 @@ package com.egoriku.grodnoroads.uidemo.ui.demo
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.egoriku.grodnoroads.foundation.core.rememberMutableState
 import com.egoriku.grodnoroads.foundation.theme.GrodnoRoadsDarkLightPreview
@@ -26,10 +28,25 @@ fun DemoRadioButton() {
                 selected = selectedOption == Option.S,
                 onClick = { selectedOption = Option.S }
             )
+            Spacer(modifier = Modifier.weight(1f))
             RadioButton(
                 selected = true,
                 onClick = {},
-                enabled = false
+            )
+            RadioButton(
+                selected = true,
+                enabled = false,
+                onClick = {},
+            )
+            RadioButton(
+                selected = false,
+                enabled = true,
+                onClick = {},
+            )
+            RadioButton(
+                selected = false,
+                enabled = false,
+                onClick = {},
             )
         }
     }
