4f20543763bdf55e962542c5fda30f1d572297c1
Yahor Urbanovich <egorikftp@gmail.com>
2023-12-07 11:15:55 +0300

    Fix import

--- app/src/main/java/com/egoriku/grodnoroads/screen/root/ui/HeadLampDialog.kt
+++ app/src/main/java/com/egoriku/grodnoroads/screen/root/ui/HeadLampDialog.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import com.egoriku.grodnoroads.foundation.dialog.DialogContent
 import com.egoriku.grodnoroads.foundation.dialog.content.DialogButton
+import com.egoriku.grodnoroads.foundation.theme.GrodnoRoadsM3ThemePreview
 import com.egoriku.grodnoroads.resources.R
 import com.egoriku.grodnoroads.screen.root.store.headlamp.HeadLampType
 import com.egoriku.grodnoroads.screen.root.store.headlamp.HeadLampType.Autumn
@@ -53,14 +54,12 @@ fun HeadLampDialog(headlampType: HeadLampType, onClose: () -> Unit) {
 
 @com.egoriku.grodnoroads.foundation.theme.GrodnoRoadsPreview
 @Composable
-private fun PreviewHeadLampDialogSpring() =
-    com.egoriku.grodnoroads.foundation.theme.GrodnoRoadsM3ThemePreview {
-        HeadLampDialog(headlampType = Spring) {}
-    }
+private fun PreviewHeadLampDialogSpring() = GrodnoRoadsM3ThemePreview {
+    HeadLampDialog(headlampType = Spring) {}
+}
 
 @com.egoriku.grodnoroads.foundation.theme.GrodnoRoadsPreview
 @Composable
-private fun PreviewHeadLampDialogAutumn() =
-    com.egoriku.grodnoroads.foundation.theme.GrodnoRoadsM3ThemePreview {
-        HeadLampDialog(headlampType = Autumn) {}
-    }
+private fun PreviewHeadLampDialogAutumn() = GrodnoRoadsM3ThemePreview {
+    HeadLampDialog(headlampType = Autumn) {}
+}
