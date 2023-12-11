175d7b610c6f83ac641fe1d5e602a67c1b248273
Yahor Urbanovich <egorikftp@gmail.com>
2023-12-11 12:30:12 +0300

    Update colors

--- compose/foundation/theme/src/main/kotlin/com/egoriku/grodnoroads/foundation/theme/Color.kt
+++ compose/foundation/theme/src/main/kotlin/com/egoriku/grodnoroads/foundation/theme/Color.kt
@@ -9,18 +9,14 @@ internal val colorUnknown = Color(0xFFE100FF)
 internal val lightColorScheme = lightColorScheme(
     primary = Color(0xFF232F34),
     onPrimary = Color.White,
-    // in figma
-    // primaryContainer = Color(0xFF29373D),
-    // onPrimaryContainer = Color(0xFFFFFFFF),
-    onPrimaryContainer = Color(0xFF29373D),
     primaryContainer = Color.White,
+    onPrimaryContainer = Color(0xFF29373D),
     inversePrimary = Color.White,
 
     secondary = Color(0xFFCC9600),
     onSecondary = Color.White,
-    secondaryContainer = Color(0xFFFFF1CC),
-    // onSecondaryContainer = Color(0xFF332500),
-    onSecondaryContainer = Color(0xFF1C1B1F),
+    secondaryContainer = Color.White,
+    onSecondaryContainer = Color(0xFF1B1B1B),
 
     tertiary = colorUnknown,
     tertiaryContainer = colorUnknown,
@@ -28,43 +24,37 @@ internal val lightColorScheme = lightColorScheme(
     onTertiaryContainer = colorUnknown,
 
     surface = Color.White,
+    surfaceVariant = Color(0xFFF4F4F5),
     onSurface = Color(0xFF232F34),
-    onSurfaceVariant = Color(0xFF5C6A70),
-    // inverseSurface = Color(0xFF2E3538),
+    surfaceContainer = Color(0xFFFDFDFD),
+    onSurfaceVariant = Color(0xFF232F34),
     inverseSurface = Color(0xFF1A1F26),
     inverseOnSurface = Color(0xFFE3E6E8),
 
     background = Color(0xFFF1F3F3),
+    onBackground = Color(0xFF1C1B1F),
 
-    outline = Color(0xFFABB5BA),
+    outline = Color(0xFFA3ACB1),
     outlineVariant = Color(0xFFE3E6E8),
 
     scrim = Color.Black,
 
     error = Color(0xFFB3261E),
     onError = Color.White,
-    errorContainer = Color(0xFFF8D5D3),
-
-    // no colors
-    onErrorContainer = colorUnknown,
-    onBackground = Color(0xFF1C1B1F),
-    surfaceVariant = Color(0xFFF4F4F5),
+    errorContainer = Color(0xFFFBEAE9),
+    onErrorContainer = Color.Black
 )
 
 internal val darkColorScheme = darkColorScheme(
     primary = Color(0xFFFFFFFF),
     onPrimary = Color(0xFF232F34),
-    // in figma
-    // primaryContainer = Color(0xFFF0F3F5),
-    // onPrimaryContainer = Color(0xFF29373D),
-    onPrimaryContainer = Color(0xFFF0F3F5),
     primaryContainer = Color(0xFF29373D),
+    onPrimaryContainer = Color.White,
     inversePrimary = Color(0xFF232F34),
 
     secondary = Color(0xFFFFC727),
     onSecondary = Color(0xFF664B00),
-    secondaryContainer = Color(0xFF997000),
-    // onSecondaryContainer = Color(0xFFFFF1CC),
+    secondaryContainer = Color(0xFF92979F),
     onSecondaryContainer = Color(0xFFFDFDFD),
 
     tertiary = colorUnknown,
@@ -72,26 +62,23 @@ internal val darkColorScheme = darkColorScheme(
     tertiaryContainer = colorUnknown,
     onTertiaryContainer = colorUnknown,
 
-    // surface = Color(0xFF2E3538),
     surface = Color(0xFF1A1F26),
+    surfaceVariant = Color(0xFF393E46),
     onSurface = Color(0xFFFDFDFD),
     onSurfaceVariant = Color(0xFFE3E6E8),
     inverseSurface = Color(0xFFE3E6E8),
     inverseOnSurface = Color(0xFF2E3538),
 
     background = Color(0xFF171A1C),
+    onBackground = Color(0xFFDDDDDD),
 
     outline = Color(0xFF454F54),
     outlineVariant = Color(0xFF5C6A70),
 
     scrim = Color.Black,
 
-    error = Color(0xFFF0ABA8),
+    error = Color(0xFFE25850),
     onError = Color(0xFF57120F),
     errorContainer = Color(0xFFAF251D),
-
-    // no colors
-    onErrorContainer = colorUnknown,
-    onBackground = Color(0xFFDDDDDD),
-    surfaceVariant = Color(0xFF393E46),
+    onErrorContainer = Color(0xFFAF251D),
 )
