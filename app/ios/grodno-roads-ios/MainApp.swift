656e97f8f8921383a4f84f26c105e744f6c060b7
Yahor Urbanovich <egorikftp@gmail.com>
2023-12-17 11:45:12 +0300

    Move firebase initialization into AppDelegate

--- app/ios/grodno-roads-ios/MainApp.swift
+++ app/ios/grodno-roads-ios/MainApp.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 import Firebase
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+}
+
 @main
 struct MainApp: App {
-    
-    init() {
-        FirebaseApp.configure()
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
