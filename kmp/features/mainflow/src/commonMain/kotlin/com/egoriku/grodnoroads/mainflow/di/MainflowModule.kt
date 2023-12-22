c5633da2eb5696936d1d4357f4ad0b863bf27a0c
Yahor Urbanovich <egorikftp@gmail.com>
2023-12-22 23:53:37 +0300

    register appearance module in koin

--- kmp/features/mainflow/src/commonMain/kotlin/com/egoriku/grodnoroads/mainflow/di/MainflowModule.kt
+++ kmp/features/mainflow/src/commonMain/kotlin/com/egoriku/grodnoroads/mainflow/di/MainflowModule.kt
@@ -1,10 +1,16 @@
 package com.egoriku.grodnoroads.mainflow.di
 
 import com.egoriku.grodnoroads.settings.alerts.di.alertsModule
+import com.egoriku.grodnoroads.settings.appearance.di.appearanceModule
 import com.egoriku.grodnoroads.settings.changelog.domain.di.changelogModule
 import com.egoriku.grodnoroads.settings.faq.di.faqModule
 import org.koin.dsl.module
 
 val mainFlowModule = module {
-    includes(alertsModule, changelogModule, faqModule)
+    includes(
+        alertsModule,
+        appearanceModule,
+        changelogModule,
+        faqModule
+    )
 }
