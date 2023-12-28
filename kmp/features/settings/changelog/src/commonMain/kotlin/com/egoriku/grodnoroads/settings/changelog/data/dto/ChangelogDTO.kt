82a427d910ab40bf7d057bcbf1868e6ccc3dca03
Yahor Urbanovich <egorikftp@gmail.com>
2023-12-28 18:15:09 +0300

    remove @JvmField

--- kmp/features/settings/changelog/src/commonMain/kotlin/com/egoriku/grodnoroads/settings/changelog/data/dto/ChangelogDTO.kt
+++ kmp/features/settings/changelog/src/commonMain/kotlin/com/egoriku/grodnoroads/settings/changelog/data/dto/ChangelogDTO.kt
@@ -3,24 +3,18 @@ package com.egoriku.grodnoroads.settings.changelog.data.dto
 import dev.gitlive.firebase.firestore.Timestamp
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlin.jvm.JvmField
 
 @Serializable
 internal class ChangelogDTO(
     @SerialName("name")
-    @JvmField
-    // TODO: remove JvmField
     val name: String,
 
     @SerialName("code")
-    @JvmField
     val code: Int,
 
     @SerialName("notes")
-    @JvmField
     val notes: String,
 
     @SerialName("date")
-    @JvmField
     val releaseDate: Timestamp
 )
--- kmp/features/settings/faq/src/commonMain/kotlin/com/egoriku/grodnoroads/settings/faq/data/dto/FaqDTO.kt
+++ kmp/features/settings/faq/src/commonMain/kotlin/com/egoriku/grodnoroads/settings/faq/data/dto/FaqDTO.kt
@@ -2,15 +2,12 @@ package com.egoriku.grodnoroads.settings.faq.data.dto
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlin.jvm.JvmField
 
 @Serializable
 internal class FaqDTO(
     @SerialName("q")
-    @JvmField
     val question: String,
 
     @SerialName("a")
-    @JvmField
     val answer: String
 )
