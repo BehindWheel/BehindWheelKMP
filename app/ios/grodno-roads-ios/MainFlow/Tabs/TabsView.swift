25f110b015ff665f2adfb9008ab21ff7ebe8ebd5
Vladislav Sitsko <vlad.sitsko@gmail.com>
2024-01-05 02:06:44 +0300

    update tabbar ui

--- app/ios/grodno-roads-ios/MainFlow/Tabs/TabsView.swift
+++ app/ios/grodno-roads-ios/MainFlow/Tabs/TabsView.swift
@@ -22,23 +22,27 @@ struct TabsView: View {
     }
     
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             ChildView(child: activeChild)
                 .frame(maxHeight: .infinity)
+            Divider()
             HStack(alignment: .bottom, spacing: 16) {
                 Button(action: { component.onSelectTab(index: 0) }) {
                     Label("Map", systemImage: "map.fill")
                         .labelStyle(VerticalLabelStyle())
                         .opacity(activeChild is TabsComponentChild.Map ? 1 : 0.5)
                 }
+                .tint(.black)
                 .frame(maxWidth: .infinity)
                 Button(action: { component.onSelectTab(index: 1) }) {
                     Label("Settings", systemImage: "gearshape.fill")
                         .labelStyle(VerticalLabelStyle())
                         .opacity(activeChild is TabsComponentChild.AppSettings ? 1 : 0.5)
                 }
+                .tint(.black)
                 .frame(maxWidth: .infinity)
             }
+            .padding(.top, 12)
         }
     }
 }
