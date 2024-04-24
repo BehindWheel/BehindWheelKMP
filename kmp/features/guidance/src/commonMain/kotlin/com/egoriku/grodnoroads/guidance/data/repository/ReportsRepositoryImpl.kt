1ae4a4593d7c80d0fb8f71d9ccd036f07b9c6bee
egorikftp <egorikftp@gmail.com>
2024-04-24 12:20:49 +0300

    add filtering logic for reports

--- kmp/features/guidance/src/commonMain/kotlin/com/egoriku/grodnoroads/guidance/data/repository/ReportsRepositoryImpl.kt
+++ kmp/features/guidance/src/commonMain/kotlin/com/egoriku/grodnoroads/guidance/data/repository/ReportsRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.egoriku.grodnoroads.guidance.data.repository
 
+import com.egoriku.grodnoroads.extensions.DateTime
 import com.egoriku.grodnoroads.extensions.common.ResultOf
 import com.egoriku.grodnoroads.guidance.data.mapper.ReportsMapper
 import com.egoriku.grodnoroads.guidance.domain.repository.ReportsRepository
@@ -10,13 +11,18 @@ import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlin.time.Duration.Companion.hours
 
-internal class ReportsRepositoryImpl(databaseReference: DatabaseReference) : ReportsRepository {
+internal class ReportsRepositoryImpl(
+    private val databaseReference: DatabaseReference
+) : ReportsRepository {
 
-    private val reportsReference = databaseReference.child("reports")
+    private val oneHourAgo = DateTime.currentTimeMillis() - 1.hours.inWholeMilliseconds
 
-    override fun loadAsFlow() = reportsReference
+    override fun loadAsFlow() = databaseReference
+        .child("reports")
         .orderByChild("timestamp")
+        .startAt(oneHourAgo.toDouble())
         .valueEvents
         .map { snapshot ->
             val dtoList = snapshot.children.map { it.value<ReportsDTO>() }
@@ -24,5 +30,4 @@ internal class ReportsRepositoryImpl(databaseReference: DatabaseReference) : Rep
         }
         .catch { ResultOf.Failure(it) }
         .flowOn(Dispatchers.IO)
-
 }
