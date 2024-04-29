cd002cad2457b908d85fbee5c909aac56337a509
egorikftp <egorikftp@gmail.com>
2024-04-29 12:31:55 +0300

    Fix icon marker size

--- app/ios/grodno-roads-ios/MainFlow/Tabs/Map/Markers/MapCameraMarker.swift
+++ app/ios/grodno-roads-ios/MainFlow/Tabs/Map/Markers/MapCameraMarker.swift
@@ -19,25 +19,12 @@ class MapCameraMarker: GMSMarker, MapMarkerProtocol {
         switch cameraType {
         case .stationary:
             self.icon = MR.images().ic_map_stationary_camera.asUIImage()
-            self.setIconSize(scaledToSize: .init(width: 26, height: 29))
         case .medium:
             self.icon = MR.images().ic_map_medium_speed_camera.asUIImage()
-            self.setIconSize(scaledToSize: .init(width: 26, height: 29))
         case .mobile:
             self.icon = MR.images().ic_map_mobile_camera.asUIImage()
-            self.setIconSize(scaledToSize: .init(width: 26, height: 29))
         case .unknown:
             self.icon = nil
         }
-    }
-}
-
-extension GMSMarker {
-    func setIconSize(scaledToSize newSize: CGSize) {
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
-        icon?.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
-        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        icon = newImage
     }
 }
