940ee3bfa48e3a78ee6b8d5cdd8a27b61046567b
egorikftp <egorikftp@gmail.com>
2024-04-29 22:33:33 +0300

    fix compose preview

--- kmp/features/settings/alerts/src/commonMain/kotlin/com/egoriku/grodnoroads/settings/alerts/domain/component/AlertsComponentPreview.kt
+++ kmp/features/settings/alerts/src/commonMain/kotlin/com/egoriku/grodnoroads/settings/alerts/domain/component/AlertsComponentPreview.kt
@@ -7,7 +7,17 @@ import com.egoriku.grodnoroads.settings.alerts.domain.component.AlertsComponent.
 class AlertsComponentPreview : AlertsComponent {
 
     override val state: CFlow<AlertState>
-        get() = flowOf(AlertState(isLoading = false))
+        get() = flowOf(
+            AlertState(
+                isLoading = false,
+                alertSettings = AlertsComponent.AlertSettings(
+                    alertAvailability = AlertsComponent.AlertsPref.AlertAvailability(
+                        alertFeatureEnabled = true,
+                        voiceAlertEnabled = true
+                    )
+                )
+            )
+        )
 
     override fun modify(pref: AlertsComponent.AlertsPref) = Unit
     override fun reset(pref: AlertsComponent.AlertsPref) = Unit
