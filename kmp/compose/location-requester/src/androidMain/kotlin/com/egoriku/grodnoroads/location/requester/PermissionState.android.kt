49b40ecf2b0135ad6581d25ae77ebf6130640ec1
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-10-21 14:15:02 +0300

    Use SideEffect to check if need to show location button

--- kmp/compose/location-requester/src/androidMain/kotlin/com/egoriku/grodnoroads/location/requester/PermissionState.android.kt
+++ kmp/compose/location-requester/src/androidMain/kotlin/com/egoriku/grodnoroads/location/requester/PermissionState.android.kt
@@ -1,29 +1,20 @@
 package com.egoriku.grodnoroads.location.requester
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.repeatOnLifecycle
 import com.egoriku.grodnoroads.foundation.core.rememberMutableState
 import com.egoriku.grodnoroads.location.requester.internal.hasLocationPermissions
 
 @Composable
 actual fun rememberLocationPermissionsState(): PermissionState {
     val context = LocalContext.current
-    val lifecycle = LocalLifecycleOwner.current.lifecycle
-
     var permissionState by rememberMutableState { PermissionState(allPermissionsGranted = false) }
 
-    LaunchedEffect(Unit) {
-        lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-            permissionState = permissionState.copy(
-                allPermissionsGranted = context.hasLocationPermissions()
-            )
-        }
+    SideEffect {
+        permissionState = permissionState.copy(allPermissionsGranted = context.hasLocationPermissions())
     }
 
     return permissionState
