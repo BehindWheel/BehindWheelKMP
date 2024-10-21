c14c24acd65e1b9405ddc9553337bb6148db100e
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-10-21 14:07:53 +0300

    Improve location handling logic

--- kmp/compose/location-requester/src/commonMain/kotlin/com/egoriku/grodnoroads/location/requester/LocationRequestStatus.kt
+++ kmp/compose/location-requester/src/commonMain/kotlin/com/egoriku/grodnoroads/location/requester/LocationRequestStatus.kt
@@ -1,10 +1,3 @@
 package com.egoriku.grodnoroads.location.requester
 
-sealed interface LocationRequestStatus {
-    data object ShowRationale : LocationRequestStatus
-    data object FineLocationDenied : LocationRequestStatus
-    data object PermissionDenied : LocationRequestStatus
-
-    data object GmsLocationEnabled : LocationRequestStatus
-    data object GmsLocationDisabled : LocationRequestStatus
-}
+expect sealed interface LocationRequestStatus
--- kmp/libraries/logger/src/iosMain/kotlin/com/egoriku/grodnoroads/logger/Logger.ios.kt
+++ kmp/libraries/logger/src/iosMain/kotlin/com/egoriku/grodnoroads/logger/Logger.ios.kt
@@ -1,5 +1,5 @@
 package com.egoriku.grodnoroads.logger
 
 actual fun logD(message: String) {
-    print("$TAG: $message")
+    println("$TAG: $message")
 }
