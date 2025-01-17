20b4c384e16cbb6fe92e4cb6ca0e7eb91fceb608
Yahor Urbanovich <yahor_urbanovich@epam.com>
2025-01-17 22:03:07 +0300

    Use stateFlow where its possible

--- kmp/features/quickSettings/src/commonMain/kotlin/com/egoriku/grodnoroads/quicksettings/domain/component/QuickSettingsComponent.kt
+++ kmp/features/quickSettings/src/commonMain/kotlin/com/egoriku/grodnoroads/quicksettings/domain/component/QuickSettingsComponent.kt
@@ -3,12 +3,12 @@ package com.egoriku.grodnoroads.quicksettings.domain.component
 import androidx.compose.runtime.Stable
 import com.egoriku.grodnoroads.quicksettings.domain.model.QuickSettingsState
 import com.egoriku.grodnoroads.quicksettings.domain.store.QuickSettingsPref
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 
 @Stable
 interface QuickSettingsComponent {
 
-    val quickSettingsState: Flow<QuickSettingsState>
+    val quickSettingsState: StateFlow<QuickSettingsState>
 
     fun updatePreferences(pref: QuickSettingsPref)
 }
