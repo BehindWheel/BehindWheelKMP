6ce8a3686d1a1d11816162a5b43447dabbcbeb4d
Yahor Urbanovich <yahor_urbanovich@epam.com>
2025-01-27 20:49:41 +0300

    Fix Point calculation for iOS, ios 1.0.6

---
+++ kmp/features/guidance/src/commonMain/kotlin/com/egoriku/grodnoroads/guidance/screen/util/OffsetUtil.kt
@@ -0,0 +1,43 @@
+package com.egoriku.grodnoroads.guidance.screen.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import com.egoriku.grodnoroads.foundation.core.LocalPlatform
+import com.egoriku.grodnoroads.foundation.core.Platform
+import com.egoriku.grodnoroads.foundation.core.Platform.Android
+import com.egoriku.grodnoroads.foundation.core.Platform.IOS
+import com.egoriku.grodnoroads.maps.compose.core.Point
+
+@Composable
+fun rememberOffsetUtil(): OffsetUtil {
+    val density = LocalDensity.current
+    val platform = LocalPlatform.current
+
+    return remember { OffsetUtil(density, platform) }
+}
+
+class OffsetUtil(
+    private val density: Density,
+    private val platform: Platform
+) {
+
+    fun offsetToPoint(offset: Offset): Point {
+        return when (platform) {
+            IOS -> {
+                Point(
+                    x = offset.x / density.density,
+                    y = offset.y / density.density
+                )
+            }
+            Android -> {
+                Point(
+                    x = offset.x,
+                    y = offset.y
+                )
+            }
+        }
+    }
+}
