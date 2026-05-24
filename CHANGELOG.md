119d249bbb7497da56d95f2d4e74fe1b29201698
egorikftp <egorikftp@gmail.com>
2026-05-24 14:17:48 +0300

    Introduce compact navigation bar

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@
 
 - [Common] Filter `Camera` alerts by road corridor: suppress alerts when user is on a parallel
   road (cross-track distance > 40 m from camera axis)
+- [Common] Introduce compact navigation bar
 - [iOS] Bump min supported iOS 16
 - [iOS] Migrate from Cocoapods to SPM
 - [iOS] Update Google Maps to 10.13.0
--- kmp/features/tabs/src/commonMain/kotlin/com/egoriku/grodnoroads/mainflow/screen/Screen.kt
+++ kmp/features/tabs/src/commonMain/kotlin/com/egoriku/grodnoroads/mainflow/screen/Screen.kt
@@ -1,6 +1,8 @@
 package com.egoriku.grodnoroads.mainflow.screen
 
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.egoriku.grodnoroads.compose.resources.Res
 import com.egoriku.grodnoroads.compose.resources.tab_map
 import com.egoriku.grodnoroads.compose.resources.tab_settings
@@ -12,13 +14,15 @@ import org.jetbrains.compose.resources.StringResource
 internal sealed class Screen(
     val index: Int,
     val stringResource: StringResource,
-    val imageVector: ImageVector
+    val imageVector: ImageVector,
+    val size: Dp = 24.dp
 ) {
 
     data object Map : Screen(
         index = 0,
         stringResource = Res.string.tab_map,
-        imageVector = GrodnoRoads.Outlined.Map
+        imageVector = GrodnoRoads.Outlined.Map,
+        size = 22.dp
     )
 
     data object AppSettings : Screen(
