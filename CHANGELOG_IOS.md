216190b923b7180a767a86b9ac57c368d08c4ca7
egorikftp <egorikftp@gmail.com>
2026-07-09 15:56:48 +0300

    Dynamic map zoom that adjusts to the selected city's default zoom level

--- CHANGELOG_IOS.md
+++ CHANGELOG_IOS.md
@@ -25,4 +25,5 @@
 - Update Google Maps to 10.13.0
 - Update medium speed camera color
 - Merge mobile cameras that are close to each other (within 300 m) into a single marker
+- Dynamic map zoom that adjusts to the selected city's default zoom level
 
--- kmp/shared/persistent/src/commonMain/kotlin/com/egoriku/grodnoroads/shared/persistent/map/location/City.kt
+++ kmp/shared/persistent/src/commonMain/kotlin/com/egoriku/grodnoroads/shared/persistent/map/location/City.kt
@@ -2,30 +2,110 @@ package com.egoriku.grodnoroads.shared.persistent.map.location
 
 import com.egoriku.grodnoroads.location.LatLng
 
-enum class City(val cityName: String, val latLng: LatLng) {
-    Berestovitca(cityName = "berestovitca", latLng = LatLng(53.191924, 24.018453)),
-    Berezovka(cityName = "berezovka", latLng = LatLng(53.718250, 25.500274)),
-    Dyatlovo(cityName = "dyatlovo", latLng = LatLng(53.465115, 25.407569)),
-    Grodno(cityName = "grodno", latLng = LatLng(53.6687765, 23.8212226)),
-    Indura(cityName = "indura", latLng = LatLng(53.459580, 23.883849)),
-    Ivie(cityName = "ivie", latLng = LatLng(53.930905, 25.770078)),
-    Korelichi(cityName = "korelichi", latLng = LatLng(53.570510, 26.139959)),
-    Lida(cityName = "lida", latLng = LatLng(53.891667, 25.302254)),
-    Mosty(cityName = "mosty", latLng = LatLng(53.413523, 24.542953)),
-    Novogrudok(cityName = "novogrudok", latLng = LatLng(53.598095, 25.825635)),
-    Ostrovec(cityName = "ostrovec", latLng = LatLng(54.615366, 25.958338)),
-    Ozery(cityName = "ozery", latLng = LatLng(53.722526, 24.178165)),
-    Porechye(cityName = "porechye", latLng = LatLng(53.885623, 24.137678)),
-    Shuchin(cityName = "shuchin", latLng = LatLng(53.604412, 24.742567)),
-    Skidel(cityName = "skidel", latLng = LatLng(53.579644, 24.237978)),
-    Slonim(cityName = "slonim", latLng = LatLng(53.092705, 25.319268)),
-    Svisloch(cityName = "svisloch", latLng = LatLng(53.035845, 24.094495)),
-    Volkovysk(cityName = "volkovysk", latLng = LatLng(53.152847, 24.444242)),
-    Voronovo(cityName = "voronovo", latLng = LatLng(54.152229, 25.319313)),
-    Zelva(cityName = "zelva", latLng = LatLng(53.149502, 24.817092)),
+enum class City(val cityName: String, val latLng: LatLng, val defaultZoom: Float) {
+    Berestovitca(
+        cityName = "berestovitca",
+        latLng = LatLng(53.191924, 24.018453),
+        defaultZoom = 14.6f
+    ),
+    Berezovka(
+        cityName = "berezovka",
+        latLng = LatLng(53.718250, 25.500274),
+        defaultZoom = 14.4f
+    ),
+    Dyatlovo(
+        cityName = "dyatlovo",
+        latLng = LatLng(53.465115, 25.407569),
+        defaultZoom = 14.3f
+    ),
+    Grodno(
+        cityName = "grodno",
+        latLng = LatLng(53.6687765, 23.8212226),
+        defaultZoom = 12.5f
+    ),
+    Indura(
+        cityName = "indura",
+        latLng = LatLng(53.459580, 23.883849),
+        defaultZoom = 15.2f
+    ),
+    Ivie(
+        cityName = "ivie",
+        latLng = LatLng(53.930905, 25.770078),
+        defaultZoom = 14.1f
+    ),
+    Korelichi(
+        cityName = "korelichi",
+        latLng = LatLng(53.570510, 26.139959),
+        defaultZoom = 14.5f
+    ),
+    Lida(
+        cityName = "lida",
+        latLng = LatLng(53.891667, 25.302254),
+        defaultZoom = 12.8f
+    ),
+    Mosty(
+        cityName = "mosty",
+        latLng = LatLng(53.413523, 24.542953),
+        defaultZoom = 13.4f
+    ),
+    Novogrudok(
+        cityName = "novogrudok",
+        latLng = LatLng(53.598095, 25.825635),
+        defaultZoom = 14.5f
+    ),
+    Ostrovec(
+        cityName = "ostrovec",
+        latLng = LatLng(54.615366, 25.958338),
+        defaultZoom = 13.4f
+    ),
+    Ozery(
+        cityName = "ozery",
+        latLng = LatLng(53.722526, 24.178165),
+        defaultZoom = 14.0f
+    ),
+    Porechye(
+        cityName = "porechye",
+        latLng = LatLng(53.884536, 24.134896),
+        defaultZoom = 14.6f
+    ),
+    Shuchin(
+        cityName = "shuchin",
+        latLng = LatLng(53.604270, 24.733359),
+        defaultZoom = 13.6f
+    ),
+    Skidel(
+        cityName = "skidel",
+        latLng = LatLng(53.581465, 24.228489),
+        defaultZoom = 12.8f
+    ),
+    Slonim(
+        cityName = "slonim",
+        latLng = LatLng(53.092705, 25.319268),
+        defaultZoom = 12.9f
+    ),
+    Svisloch(
+        cityName = "svisloch",
+        latLng = LatLng(53.035845, 24.094495),
+        defaultZoom = 14.5f
+    ),
+    Volkovysk(
+        cityName = "volkovysk",
+        latLng = LatLng(53.152847, 24.444242),
+        defaultZoom = 13.0f
+    ),
+    Voronovo(
+        cityName = "voronovo",
+        latLng = LatLng(54.150707, 25.313543),
+        defaultZoom = 13.9f
+    ),
+    Zelva(
+        cityName = "zelva",
+        latLng = LatLng(53.149223, 24.813549),
+        defaultZoom = 14.3f
+    ),
 
     // Brest region
-    Ruzhany(cityName = "ruzhany", latLng = LatLng(52.866458, 24.890205));
+    Ruzhany(cityName = "ruzhany", latLng = LatLng(52.866458, 24.890205), defaultZoom = 14.5f);
 
     companion object {
         fun toCity(value: String) = checkNotNull(entries.find { it.cityName == value })
